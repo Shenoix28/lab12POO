c2639c1f2b4b2f6a082964b9a0e4ba0a4afd17bc
mols <mols1@live.com>
2021-11-05 15:58:14 -0600

    Write XML

--- src/controlador/test.java
+++ src/controlador/test.java
@@ -7,17 +7,9 @@ public class test {
 	public static void main(String[] args){
 		
 		
-		String[] usuarios=DatosXML.getElements("UsuariosDAO","UsuarioDAO","nombreUsuario");
+		DatosXML.writeElements("UsuariosDAO","UsuarioDAO","nombreUsuario");
 		
-		System.out.println(usuarios[0]);
-		System.out.println(usuarios[1]);
-		System.out.println(usuarios[2]);
 		
-		String[] contraseñas=DatosXML.getElements("UsuariosDAO","UsuarioDAO","contrasenna");
-
-		System.out.println(contraseñas[0]);
-		System.out.println(contraseñas[1]);
-		System.out.println(contraseñas[2]);
 	}
 
 }
--- src/modelo/DatosXML.java
+++ src/modelo/DatosXML.java
@@ -1,14 +1,23 @@
 package modelo;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -17,7 +26,7 @@ public class DatosXML {
 	private static Document doc;
 	private static DocumentBuilder builder;
 
-	public static String[] getElements(String fileName,String objectName, String atributo) {
+	public static String[] getElements(String fileName, String objectName, String atributo) {
 		File inputFile = new File(fileName + ".xml");
 		String[] datos;
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -32,18 +41,46 @@ public class DatosXML {
 		}
 
 		NodeList usuarios = doc.getElementsByTagName(objectName);
-		datos=new String[usuarios.getLength()];
+		datos = new String[usuarios.getLength()];
 		for (int i = 0; usuarios.getLength() > i; i++) {
 			Element usuario = (Element) usuarios.item(i);
-			datos[i]=usuario.getElementsByTagName(atributo).item(0).getTextContent();
+			datos[i] = usuario.getElementsByTagName(atributo).item(0).getTextContent();
 		}
-		
+
 		return datos;
 
 	}
-	
-	public static void writeElements(String fileName,String objectName, String atributo) {
-		
+
+	public static void writeElements(String fileName, String objectName, String atributo) {
+		File inputFile = new File(fileName + ".xml");
+		String[] datos;
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+			builder = factory.newDocumentBuilder();
+			doc = builder.parse(inputFile);
+			//agrega al objeto
+			Element objeto=doc.createElement(objectName);      
+			//agrega atributos
+			Node data=doc.createTextNode("nombreUsuario");
+			data.setNodeValue("numa");
+			objeto.appendChild(data);
+			
+			doc.getDocumentElement().appendChild(objeto);
+			
+
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new FileOutputStream(fileName + ".xml"));
+
+			TransformerFactory transFactory = TransformerFactory.newInstance();
+			Transformer transformer = transFactory.newTransformer();
+
+			transformer.transform(source, result);
+			
+		} catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
